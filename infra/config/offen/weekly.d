42c8dddbea6f43c9812271ed4966d9aa5a1a0e09
Catalin Modan <contact@cmodan.ro>
2024-12-21 20:34:21 +0200

    feat: adjust retention period

--- infra/config/offen/weekly.d
+++ infra/config/offen/weekly.d
@@ -2,4 +2,4 @@ BACKUP_FILENAME="weekly-backup-%Y-%m-%dT%H-%M-%S.tar.gz"
 # run every monday at 3am
 BACKUP_CRON_EXPRESSION="0 3 * * 1"
 BACKUP_PRUNING_PREFIX="weekly-backup-"
-BACKUP_RETENTION_DAYS="31"
+BACKUP_RETENTION_DAYS="14"
